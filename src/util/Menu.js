62cb5e33f3a9dd7918e53b079dc75a7eeddb1a84
James Dunseith <info@workshopmaybe.com>
2020-12-12 11:23:07 -0500

    Improve mobile view of menu and socials

--- src/util/Menu.js
+++ src/util/Menu.js
@@ -1,34 +1,76 @@
-import React from 'react'
+import { React, Fragment } from 'react'
 
 function Menu() {
     return (
-        <div
-        className="menu"
-        style={{
-        position: 'fixed',    
-        top: 100,
-        left: 0,
-        zIndex: 2000,
-        background: 'rgba(66,31,113,0.3)',
-        }}
-        >
 
-            <ul class="nav flex-column">
-            <li class="nav-item border-0">
-                <a class="nav-link" data-menuanchor="home" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-menuanchor="about" href="#about">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-menuanchor="delegate" href="#delegate">Delegate</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-menuanchor="partners" href="#partners">Partners</a>
-            </li>         
-            </ul>
-        </div>
+        <Fragment>
+            <div class="d-block d-lg-none">
+                <div
+                className="menu"
+                style={{
+                position: 'fixed',    
+                top: 8,
+                left: 0,
+                width: '100%',
+                zIndex: 2000,
+                background: 'rgba(66,31,113,0.3)',
+                }}
+                >
+
+                    <ul class="nav nav-fill">
+                        <li class="nav-item border-0">
+                            <a class="nav-link" data-menuanchor="home" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-menuanchor="about" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-menuanchor="delegate" href="#delegate">Delegate</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-menuanchor="partners" href="#partners">Partners</a>
+                        </li>         
+                    </ul>
+                </div>
+            </div>
+
+            <div class="d-none d-lg-block">
+                <div
+                className="menu"
+                style={{
+                position: 'fixed',    
+                top: 100,
+                left: 0,
+                zIndex: 2000,
+                background: 'rgba(66,31,113,0.3)',
+                }}
+                >
+
+                    <ul class="nav flex-column">
+                        <li class="nav-item border-0">
+                            <a class="nav-link" data-menuanchor="home" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-menuanchor="about" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-menuanchor="delegate" href="#delegate">Delegate</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-menuanchor="partners" href="#partners">Partners</a>
+                        </li>         
+                    </ul>
+                </div>
+            </div>
+        </Fragment>
+
+
     );
 }
+
+
+
+
+
 
 export default Menu
--- src/util/Social.js
+++ src/util/Social.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, Fragment } from 'react';
 
 import csocial from '../images/cardano-social.png';
 import instagram from '../images/instagram.png';
@@ -8,34 +8,67 @@ import styled from 'styled-components';
 
 function Social() {
     return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid" style={{
-            position: 'fixed',    
-            bottom: 20,
-            width: '33%',
-            right: 0,
-            zIndex: 2000,
-            background: 'rgba(5,108,242,0.3)',        
-        }}>
-            <ul class="navbar-nav">
-                <li class="nav-item"><a href=""><Img src={csocial} /></a></li>
-                <li class="nav-item"><a href=""><Img src={instagram} /></a></li>
-                <li class="nav-item"><a href="https://twitter.com/zoepoolz"><Img src={twitter} /></a></li>
-                <li class="nav-item"><a href=""><Img src={medium} /></a></li>
-                        
-            </ul>       
-        </div>
-    </nav>
+    
+
+
+
+        <Fragment>
+            <div class="d-block d-lg-none">
+                
+                    <div className="container-fluid" style={{
+                        position: 'fixed',    
+                        bottom: 8,
+                        width: '100%',
+                        right: 0,
+                        zIndex: 2000,
+                        background: 'rgba(5,108,242,0.3)',        
+                    }}>
+                        <ul class="nav nav-fill">
+                            <li class="nav-item"><a href=""><ImgM src={csocial} /></a></li>
+                            <li class="nav-item"><a href=""><ImgM src={instagram} /></a></li>
+                            <li class="nav-item"><a href="https://twitter.com/zoepoolz"><ImgM src={twitter} /></a></li>
+                            <li class="nav-item"><a href=""><ImgM src={medium} /></a></li>            
+                        </ul>       
+                    </div>
+               
+            </div>
+            <div class="d-none d-lg-block">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid" style={{
+                        position: 'fixed',    
+                        bottom: 20,
+                        width: '50%',
+                        right: 0,
+                        zIndex: 2000,
+                        background: 'rgba(5,108,242,0.3)',        
+                    }}>
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a href=""><Img src={csocial} /></a></li>
+                            <li class="nav-item"><a href=""><Img src={instagram} /></a></li>
+                            <li class="nav-item"><a href="https://twitter.com/zoepoolz"><Img src={twitter} /></a></li>
+                            <li class="nav-item"><a href=""><Img src={medium} /></a></li>            
+                        </ul>       
+                    </div>
+                </nav>
+            </div>
+        </Fragment>
     
 
     )
 }
 
 const Img = styled.img`
-    height: 70px;
+    height: 100px;
     padding-left: 1em;
     padding-bottom: 0.5em;
     padding-top: 0.5em;
+`;
+
+const ImgM = styled.img`
+    height: 35px;
+    padding-left: 0.1em;
+    padding-bottom: 0.1em;
+    padding-top: 0.1em;
 `;
 
 
